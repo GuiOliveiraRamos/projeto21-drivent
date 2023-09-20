6a5b8bf623e35507cb9a2f8a7a761b8428120597
GuiOliveiraRamos <guioliveiraramos30@gmail.com>
2023-09-20 11:15:29 -0300

    fixed: authenticatedRequest

--- src/controllers/tickets-controller.ts
+++ src/controllers/tickets-controller.ts
@@ -1,8 +1,9 @@
-import { Request, Response } from 'express';
+import { Response } from 'express';
 import httpStatus from 'http-status';
 import { ticketsService } from '@/services/tickets-service';
+import { AuthenticatedRequest } from '@/middlewares';
 
-export async function getTicketsTypes(req: Request, res: Response) {
+export async function getTicketsTypes(req: AuthenticatedRequest, res: Response) {
   const getAllTicketsTypes = await ticketsService.getTicketsTypes();
   res.status(httpStatus.OK).send(getAllTicketsTypes);
 }
