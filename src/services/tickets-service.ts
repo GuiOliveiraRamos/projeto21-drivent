c47e15727f92c86017fdebc010ccf54aad7d0f5d
GuiOliveiraRamos <guioliveiraramos30@gmail.com>
2023-09-22 16:16:02 -0300

    fix: bugs

--- src/services/tickets-service.ts
+++ src/services/tickets-service.ts
@@ -1,6 +1,8 @@
-import { Ticket, TicketType } from '@prisma/client';
-import { notFoundError } from '@/errors';
+import { Ticket, TicketStatus, TicketType } from '@prisma/client';
+import { notFoundError, requestError } from '@/errors';
 import { ticketsRepository } from '@/repositories/tickets-repository';
+import { TicketData } from '@/protocols';
+import { enrollmentRepository } from '@/repositories';
 
 async function getTicketsTypes(): Promise<TicketType[]> {
   const getAllTicketsTypes: TicketType[] = await ticketsRepository.getTicketsTypes();
@@ -16,6 +18,25 @@ async function getUserTicket(userId: number): Promise<Ticket> {
   if (!verifyTicket) throw notFoundError();
 
   return verifyTicket;
+}
+
+async function postNewTicket(userId: number, ticketTypeId: number): Promise<Ticket> {
+  const verifyEnrollment = await enrollmentRepository.findWithAddressByUserId(userId);
+  if (!verifyEnrollment) throw notFoundError();
+
+  if (!ticketTypeId) throw requestError(400, 'Ticket type ID is required');
+
+  const ticketData: TicketData = {
+    ticketTypeId,
+    enrollmentId: verifyEnrollment.id,
+    status: TicketStatus.RESERVED,
+  };
+
+  await ticketsRepository.createTicket(ticketData);
+
+  const ticket = await ticketsRepository.findUserEnrollment(verifyEnrollment.id);
+
+  return ticket;
 }
 
 export const ticketsService = {
