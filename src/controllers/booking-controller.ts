750ebc52f630fdae881d21b638c61b4ca177d4c0
GuiOliveiraRamos <guioliveiraramos30@gmail.com>
2023-10-04 15:13:12 -0300

    feat: edit booking

--- src/controllers/booking-controller.ts
+++ src/controllers/booking-controller.ts
@@ -1,6 +1,6 @@
 import { Response } from 'express';
 import httpStatus from 'http-status';
-import { notFoundError } from '@/errors';
+import { notFoundError, requestError } from '@/errors';
 import { AuthenticatedRequest } from '@/middlewares';
 import bookingService from '@/services/booking-service';
 
@@ -17,5 +17,16 @@ export async function postBooking(req: AuthenticatedRequest, res: Response) {
   if (!roomId) throw notFoundError();
 
   const result = await bookingService.postBooking(userId, roomId);
+  res.status(httpStatus.OK).send(result);
+}
+
+export async function editBooking(req: AuthenticatedRequest, res: Response) {
+  const { userId } = req;
+  const { roomId } = req.body;
+  const bookingId = Number(req.params.bookingId);
+
+  if (bookingId <= 0 || isNaN(bookingId)) throw requestError(400, 'invalid bookingId');
+
+  const result = await bookingService.editBooking(userId, roomId, bookingId);
   res.status(httpStatus.OK).send(result);
 }
--- src/services/booking-service.ts
+++ src/services/booking-service.ts
@@ -44,9 +44,35 @@ async function postBooking(userId: number, roomId: number) {
   };
 }
 
+async function editBooking(userId: number, roomId: number, bookingId: number) {
+  const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
+  if (!enrollment) throw notFoundError();
+
+  const ticket = await ticketsRepository.findTicketByEnrollmentId(enrollment.id);
+  if (!ticket) throw notFoundError();
+
+  const findBooking = await bookingRepository.getBookings(userId);
+  if (!findBooking || findBooking.id !== bookingId) throw error(403, 'forbidden action');
+  // ARQUITETAR O ERRO
+
+  const checkRoom = await hotelRepository.findRoomById(roomId);
+  if (!checkRoom) throw notFoundError();
+  if (checkRoom.Booking.length >= checkRoom.capacity) {
+    throw error(403, 'forbidden action');
+    // ARQUITETAR O ERRO
+  }
+
+  const editBooking = await bookingRepository.editBooking(userId, roomId);
+
+  return {
+    bookingId: editBooking.id,
+  };
+}
+
 const bookingService = {
   getBookings,
   postBooking,
+  editBooking,
 };
 
 export default bookingService;
